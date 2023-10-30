7ae2acb8781f75b2d1d5930846d1a14a484aefa0
youngil <youngil@placeb.com>
2023-10-30 11:14:05 +0900

    플레이어 이동

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,43 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField]
+    float moveSpeed = 1f;
+
+    [SerializeField]
+    float rotateSpeed = 1f;
+
+    Vector3 moveValue;
+
+    Animator animator;
+
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    void OnMove(InputValue value)
+    {
+        var inputValue = value.Get<Vector2>();
+
+        moveValue = new Vector3(inputValue.x, 0, inputValue.y);
+        bool isMoving = moveValue != Vector3.zero;
+        animator.SetBool("move", isMoving);
+    }
+
+    private void Update()
+    {
+        transform.position += moveSpeed * Time.deltaTime * moveValue;        
+        if(moveValue != Vector3.zero)
+        {
+            var rotValue = moveValue;
+            rotValue.Normalize();
+
+            Quaternion targetRotation = Quaternion.LookRotation(rotValue);
+            transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, rotateSpeed * Time.deltaTime);
+        }
+    }
+
+}
